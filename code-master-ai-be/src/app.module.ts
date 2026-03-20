c3566615a3c6e954a77922268efcd9d466f5e086
dinhsang-28 <Buidinhsang2806@gmail.com>
2026-03-20 21:29:53 +0700

    config-mongo-main

--- code-master-ai-be/src/app.module.ts
+++ code-master-ai-be/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [
+    // nap bien cuc bo .env
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    // cau hinh ket noi mongodb voi cac tuy chon va toi uu va bao mat
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async (configService:ConfigService)=>({
+        uri:configService.get<string>('MONGODB_URI'),
+      }),
+      inject:[ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
