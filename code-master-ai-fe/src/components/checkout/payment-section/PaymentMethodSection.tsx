2ba62e45072e56ab547f1db12cf7f82edff4dd63
Le Anh Khoa <khoale22082004@gmail.com>
2026-04-04 17:17:09 +0700

    add logo payment method

--- code-master-ai-fe/src/components/checkout/payment-section/PaymentMethodSection.tsx
+++ code-master-ai-fe/src/components/checkout/payment-section/PaymentMethodSection.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import MomoLogo from "../../../assets/MomoLogo.png";
+import VNPayLogo from "../../../assets/VNPayLogo.jpg";
 import { Wallet, CircleCheck } from "lucide-react";
 import type { CheckoutFormData } from "../../../types/checkout/checkout";
 
@@ -14,13 +16,13 @@ const PaymentMethodSection = ({
   const methods = [
     {
       value: "momo",
-      icon: "Ví Momo",
+      icon: MomoLogo,
       title: "Ví Momo",
       // desc: "Vietcombank, Techcombank, MB Bank...",
     },
     {
       value: "vnpay",
-      icon: "Ví VNPay",
+      icon: VNPayLogo,
       title: "Ví VNPay",
       // desc: "Momo, ZaloPay, ShopeePay",
     },
@@ -59,12 +61,24 @@ const PaymentMethodSection = ({
                   onChange={() => onChange("paymentMethod", method.value)}
                 />
                 <div className="flex items-center gap-4">
-                  <span
+                  {/* <span
                     className={`material-symbols-outlined text-slate-600 p-2 rounded-md ${
                       active ? "bg-white shadow-sm" : "bg-slate-100"
                     }`}
                   >
                     {method.icon}
+                  </span> */}
+
+                  <span
+                    className={`p-2 rounded-md flex items-center justify-center ${
+                      active ? "bg-white shadow-sm" : "bg-slate-100"
+                    }`}
+                  >
+                    <img
+                      src={method.icon}
+                      alt={method.title}
+                      className="w-8 h-8 object-contain"
+                    />
                   </span>
                   <div>
                     <p className="font-bold text-slate-900">{method.title}</p>
